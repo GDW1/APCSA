168244a0aa3d63652f1331e4e1f2d9e82547fdfa
Guy <798932@seq.org>
2018-10-25 10:30:20 -0700

    student

---
+++ Projects/StudentList/Student.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    // instance variables - replace the example below with your own
+    double gpa;
+    int stuNumber;
+    String name;
+    
+    /**
+     * Constructor for objects of class Student
+     */
+    public Student()
+    {
+        // initialise instance variables
+
+    }
+}
---
+++ Projects/StudentList/StudentList.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class StudentList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class StudentList extends Student
+{
+    ArrayList<Integer> mainList;
+    /**
+     * Constructor for objects of class StudentList
+     */
+    public StudentList()
+    {
+        // initialise instance variables
+        
+    }
+    public static void addStudent(){
+    
+    }
+    public static void deleteStudent(){
+    
+    }
+    public static void editStudentList(){
+    
+    }
+    public static void clearList(){
+    
+    }
+    public static void printList(){
+    
+    }
+    public static void printStudent(){
+    
+    }
+}
