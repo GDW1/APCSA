3bee7d85e305912b9dcc38f7a8f42af036a9ca84
GDW1 <31372594+GDW1@users.noreply.github.com>
2018-10-29 10:26:17 -0700

    Add files via upload

--- Projects/StudentList/StudentList.java
+++ Projects/StudentList/StudentList.java
@@ -6,33 +6,98 @@
  * @version (a version number or a date)
  */
 import java.util.*;
-public class StudentList extends Student
+public class StudentList
 {
-    ArrayList<Integer> mainList;
+    ArrayList<Student> mainList;
     /**
      * Constructor for objects of class StudentList
      */
     public StudentList()
     {
         // initialise instance variables
-        
+        mainList = new ArrayList<Student>();
     }
-    public static void addStudent(){
-    
+    public void addStudent(String name, double gpa, int stuNumber){
+        mainList.add(new Student(name, stuNumber, gpa));
     }
-    public static void deleteStudent(){
-    
+    public void deleteStudent(String lastName){
+        String tempLastName = lastName;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getLastName()){
+                mainList.remove(i);
+            }
+        }
     }
-    public static void editStudentList(){
-    
+    public void editStudentList(int number, String editedVar){
+        int tempLastName = number;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getNum()){
+                mainList.get(i).setName(editedVar);
+            }
+        }
     }
-    public static void clearList(){
-    
+    public void editStudentList(String name, String editedVar){
+        String tempLastName = name;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getFullName()){
+                mainList.get(i).setName(editedVar);
+            }
+        }
     }
-    public static void printList(){
-    
+    public void editStudentList(int number, int editedVar){
+        int tempLastName = number;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getNum()){
+                mainList.get(i).setNum(editedVar);
+            }
+        }
     }
-    public static void printStudent(){
-    
+    public void editStudentList(String name, int editedVar){
+        String tempLastName = name;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getFullName()){
+                mainList.get(i).setNum(editedVar);
+            }
+        }
+    }
+    public void editStudentList(int number, double editedVar){
+        int tempLastName = number;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getNum()){
+                mainList.get(i).setGpa(editedVar);
+            }
+        }
+    }
+    public void editStudentList(String name, double editedVar){
+        String tempLastName = name;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getFullName()){
+                mainList.get(i).setGpa(editedVar);
+            }
+        }
+    }
+    public void clearList(){
+        mainList.clear();
+    }
+    public void printList(){
+        for(int i = 0; i < mainList.size(); i++){
+            System.out.println(mainList.get(i).getFirstName());
+        }
+    }
+    public void printStudent(String name){
+        String tempLastName = name;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getFullName()){
+                System.out.println("Student Name:" + mainList.get(i).getFullName());
+            }
+        }
+    }
+    public void printStudent(int number){
+        int tempLastName = number;
+        for(int i = 0; i < mainList.size(); i++){
+            if(tempLastName == mainList.get(i).getNum()){
+                System.out.println("Student Num:" + mainList.get(i).getNum());
+            }
+        }
     }
 }
--- Projects/StudentList/StudentListRunner.java
+++ Projects/StudentList/StudentListRunner.java
@@ -5,11 +5,49 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
 public class StudentListRunner extends StudentList
 {
+    static StudentList list = new StudentList();
     // instance variables - replace the example below with your own
     public static void main(){
+        StudentListRunner runner = new StudentListRunner();
         StudentList studlist = new StudentList(); 
-        
+        runner.giveList();
+    }
+    public void giveList(){
+        StudentListRunner runner = new StudentListRunner();
+        System.out.println("Pick an option from the list below\n 1 - Add Student \n 2 - Edit Student \n 3 - Delete Student \n 4 - Delete All Students \n 5 - Print one Student  \n 6 - Print All Students");
+        Scanner pickFromList = new Scanner(System.in);
+        System.out.print("Pick a number from the options to begin: ");
+        int numPicked = pickFromList.nextInt();
+        System.out.println(numPicked);
+        switch(numPicked){
+            case 0:
+                runner.addStudentInput();
+            break;
+            case 1:
+            
+            break;
+            case 2:
+            
+            break;
+            case 3:
+            
+            break;
+            case 4:
+            
+            break;
+            case 5:
+            
+            break;
+            case 6:
+            
+            break;
+            
+        }
+    }
+    public void addStudentInput(){
+    
     }
 }
