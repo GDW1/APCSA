86c1790c0998ce91fb2cfafb3f430a839f4ba62a
Guy <798932@seq.org>
2018-11-15 10:17:42 -0800

    commit

--- Projects/Project  5 - Pix Lab/pixLab/classes/PictureExplorer.java
+++ Projects/Project  5 - Pix Lab/pixLab/classes/PictureExplorer.java
@@ -800,7 +800,9 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("swan.jpg");
+    //Picture smallP = pix.scale(0.25,0.25);
+    //smallP.write("smallMyPic");
     pix.explore();
   }
   
